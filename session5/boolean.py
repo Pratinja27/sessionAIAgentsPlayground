55819c83bcff23107c67d74145a0fb8cba17bb5c
Pratinja27 <pratinjavaidya77gmail.com>
2026-01-27 21:11:55 +0530

    boolean done

---
+++ session5/boolean.py
@@ -0,0 +1,12 @@
+is_logged_in=True
+if is_logged_in:
+    
+    print("hello user!")
+    
+    
+atm_pin=1234
+entered_pin=1111
+if entered_pin==atm_pin:
+    print("correct pin")
+else:
+    print("pin wrong")
---
+++ session5/conditional.py
@@ -0,0 +1,23 @@
+marks=50
+if marks>=40:
+    print("student is passed")
+    
+else:
+    print("fail:(")
+
+
+
+age=21
+if age>=18:
+    print("ur an adult")
+elif age>=20:
+    print("ur eligible to drink")
+else:
+    print("done")
+
+amount=1200
+if amount>=1100:
+    disamount=amount-100
+    print("discounted amount is",disamount)
+else:
+    print("no discount")
