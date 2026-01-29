45467821d2f7bff17fa5b5984336a25119ae7d2c
Pratinja27 <pratinjavaidya77gmail.com>
2026-01-29 20:48:19 +0530

    range based loops

---
+++ session5/logical_opeators.py
@@ -0,0 +1,44 @@
+age=20
+has_address=True
+if age>=18 and has_address==True:
+    print("entry allowed")
+    
+else:
+    print("entry denied")
+# AND operator
+    
+balance=500
+price=300
+is_user_ac_exists=True
+
+if balance>=price and price>0 and is_user_ac_exists :
+    print("order placed")
+else:
+    print("order cant be place")
+    
+    
+    
+# OR operator
+login_google=False
+login_email=True
+
+if login_google or login_email:
+    print("has logged in")
+    
+    
+    
+is_owner=True
+is_admin=False
+
+
+if is_owner or is_admin:
+    print("access granted")
+else:
+    print("access denied")   
+    
+    
+    
+
+# not operator
+
+# reverse the boolean
