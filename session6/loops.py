fa4b7bfbf68d5993fa6d87786d85858af501a903
Pratinja27 <pratinjavaidya77gmail.com>
2026-01-29 20:48:43 +0530

    range based loops

---
+++ session6/loops.py
@@ -0,0 +1,18 @@
+# checking an sheets
+# take one paper
+# move to next
+# stop when paper is done
+
+# what 'i' is just to store index
+
+# for x in range(-3,5):
+#     print(x)
+    
+    
+for i in range(5,-3,-1):
+    print(i)
+    
+    
+# range funtion
+
+# for i in range(0 ,10)Lo
