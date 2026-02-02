71ea5ab3103fb1de0d1860dd5829ee894af2fb96
Pratinja27 <pratinjavaidya77gmail.com>
2026-02-02 20:41:49 +0530

    function basics

---
+++ session7/fun.py
@@ -0,0 +1,20 @@
+def greet():
+    print("hello my function")
+    
+greet()
+
+# parameters and arguments
+
+def greeting(name):
+    print("hello",name)
+    
+    
+greeting("shubhendu")
+
+# calculate price of our product whos unit price is 300rs and quntity is 3
+
+def cal(product,quatity):
+    price=product*quatity
+    print(price)
+
+cal(300,3)
