35690495a54e3d0ea120118de8115a7350e2b422
Pratinja27 <pratinjavaidya77@gmail.com>
2026-02-17 20:37:03 +0530

    added link of notes

--- session10/example.py
+++ session10/example.py
@@ -2,3 +2,4 @@ import numpy as np
 arr=np.array([1,2,3,4])
 
 print(arr)
+# https://docs.google.com/document/d/1vOHMUMNyujn2QOpW6dHrAfz0Y7oKoaBYBRt1SLI1LWA/edit?tab=t.mo7w5q2g9qkj#heading=h.31u420qq2h7w
